225057e683a832c9cc070c77c46dbd6e91260ac0
clcost <clcost@crimson.ua.edu>
2019-03-07 16:57:41 -0600

    changes made to main.c

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+//main.c for cs201Project written by Coleman Cost
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -5,7 +6,7 @@
 
 int main (void) {
 
-	char uniqueId[20];
+	char uniqueId[20];					//temporary variables for movie until stored in data structure
 	char titleType[30];
 	char primTitle[500];
 	char origTitle[500];
@@ -19,64 +20,60 @@ int main (void) {
 	int count = 0;
 	int i = 0;
 
-	FILE *fp = fopen("sample.basics.tsv", "r");
+	FILE *fp = fopen("movie_records_sample", "r");
  		
-		fscanf(fp, "%s", uniqueId); printf("|%s|\n", uniqueId);
-		fscanf(fp, "%s", titleType); printf("|%s|\n", titleType);
+		fscanf(fp, "%s", uniqueId);			//scanning in uniqueId and titleType from file
+		fscanf(fp, "%s", titleType);
 		temp = fgetc(fp);
 	
 	while (1) {
-		//fscanf(fp, "%s", uniqueId);
-		//fscanf(fp, "%s", titleType);
 		i = 0;
-		//temp = fgetc(fp);
 		
-		while (1) {
-			if (!isspace(temp)) break; 
+		while (1) {					//advancing fp to next word
+			if (isalpha(temp)) break; 
 			temp = fgetc(fp);
 		}
-		printf("ffs\n");
-		while (1) {
+
+		while (1) {					//reading primTitle in char by char (in order to include spaces)
 			if (temp == '\t') break;
 			primTitle[i] = temp;
 			i = i + 1;
 			temp = fgetc(fp);
 		}
 	
-		primTitle[i] = '\0'; printf("[%s]\n", primTitle);
+		primTitle[i] = '\0';
 		i = 0;
-		temp = fgetc(fp);
 
-		while (1) {
-			if (!isspace(temp)) break;
+		while (1) {					//advancing fp to next title
+			if (isalpha(temp)) break;
 			temp = fgetc(fp);
 		}
 
-		while (1) {
+		while (1) {					//reading origTitle in char by char (in order to include spaces)
 			if (temp == '\t') break;
 			origTitle[i] = temp;
 			i = i + 1;
 			temp = fgetc(fp);
 		}
-		origTitle[i] = '\0'; printf("[%s]\n", origTitle);
-		//i = 0;
-		//temp = fgetc(fp);
+		origTitle[i] = '\0';
 
-		fscanf(fp, "%s", adultFilm); printf("(%s)\n", adultFilm);
+		fscanf(fp, "%s", adultFilm);			//reading in the rest of the variables from file
 		fscanf(fp, "%s", startYear);
 		fscanf(fp, "%s", endYear);
 		fscanf(fp, "%s", runTime);
-		fscanf(fp, "%s", genre); printf("(%s)\n", genre);
+		fscanf(fp, "%s", genre);
 
-		printf("%s %s %s %s %s %s %s %s %s\n", uniqueId, titleType, primTitle, origTitle, adultFilm, startYear, endYear, runTime, genre);
+		printf("%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n", uniqueId, titleType, primTitle, origTitle, adultFilm, startYear, endYear, runTime, genre); //testing the variables that were just read in
 		count = count + 1;
+		
 		temp = fgetc(fp);
-		if (temp == -1) {printf("%d BROKE THE LOOP\n", count); break; }
+		fscanf(fp, "%s", uniqueId);			//read first catagory again
 
-		fscanf(fp, "%s", uniqueId);
-		fscanf(fp, "%s", titleType);
+		if (feof(fp)) {printf("%d BROKE THE LOOP\n", count); break; } //if we are at end of file, break the loop
+
+		fscanf(fp, "%s", titleType); 			//read second catagory again
 	}
-	printf("%d\n", count);
+
 	fclose(fp);
 
 	return 0;
